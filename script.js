75d6e20c7a1e1d62271940a411ea43a37c481f43
2niG <168422030+2niG@users.noreply.github.com>
2024-06-30 11:14:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var recettes = {
   entree: [
     {
-      id: "salade",
+      id: "saladecesar",
       nom: "Salade César",
       ingredients: ["laitue", "parmesan", "croûtons", "sauce César"],
       instructions: "Mélangez tous les ingrédients dans un grand bol.",
@@ -10,7 +10,23 @@ var recettes = {
   ],
   plat: [
     {
-      id: "gratin",
+      id: "cake_thon",
+      nom: "Cake au Thon",
+      ingredients: [
+        "200g de thon en boite",
+        "100g de gruyere rapé",
+        "100g de farine",
+        "1/2 sachet de levure",
+        "3 oeufs",
+        "8cl de lait",
+        "10cl d'huile",
+        "1 pincée de poivre",
+      ],
+      instructions:
+        "Mélangez tous les ingrédients dans un saladier, en incorporant le thon et le gruyére en dernier. Versez le mélange dans un moule à cake beurré et fariné. Faites cuire 40 à 45mn au four thermostat 6-7 (180°C-210°C)",
+    },
+    {
+      id: "gratin_pates",
       nom: "Gratin de pâtes au jambon façon Cyril Lignac",
       ingredients: [
         "200g de Coquillettes",
@@ -23,7 +39,7 @@ var recettes = {
         "Poivre",
       ],
       instructions:
-        "Préparez la béchamel : faites fondre le beurre dans une casserole. Ajoutez la farine et mélangez avec une cuillère en bois. Versez le lait en filet, sans cesser de fouetter, puis faites épaissir sur feu doux pendant environ 10 min en remuant régulièrement. Salez et poivrez. Faites cuire les coquillettes al dente dans une casserole d’eau bouillante salée. Égouttez. Préchauffez le four à 210° (th.7). Versez les pâtes dans un plat à gratin, ajoutez le jambon coupé en lanières et la béchamel puis mélangez. Couvrez d’emmental râpé, puis enfournez et laissez gratiner environ 15 min. Servez bien chaud[^1^][1].",
+        "Préparez la béchamel : faites fondre le beurre dans une casserole. Ajoutez la farine et mélangez avec une cuillère en bois. Versez le lait en filet, sans cesser de fouetter, puis faites épaissir sur feu doux pendant environ 10 min en remuant régulièrement. Salez et poivrez. Faites cuire les coquillettes al dente dans une casserole d’eau bouillante salée. Égouttez. Préchauffez le four à 210° (th.7). Versez les pâtes dans un plat à gratin, ajoutez le jambon coupé en lanières et la béchamel puis mélangez. Couvrez d’emmental râpé, puis enfournez et laissez gratiner environ 15 min. Servez bien chaud.",
     },
     {
       id: "souffle_fromage",
@@ -45,7 +61,7 @@ var recettes = {
   ],
   dessert: [
     {
-      id: "crepes",
+      id: "crepes_biere",
       nom: "Crêpes",
       ingredients: [
         "300g de farine",
@@ -59,7 +75,7 @@ var recettes = {
         "Dans un saladier, versez la farine et formez un puits, versez le lait en plusieurs fois dans ce puits. Mélangez en faisant tomber la farine petit à petit dans le lait afin de l'incorporer sans créer de grumeaux. Ajoutez ensuite la bière et mélangez de nouveau. Battez les œufs puis incorporez-les à la pâte avec l'huile. Versez enfin le sucre et mélangez bien. Laissez la pâte reposer 1 heure minimum (plus elle reposera mieux ça sera). Après ce temps de repos, mélangez un peu de nouveau et faites cuire vos crêpes dans une poêle à crêpes huilée.",
     },
     {
-      id: "gateau",
+      id: "gateau_savoie",
       nom: "Gâteau de Savoie",
       ingredients: [
         "125g de farine",
@@ -71,21 +87,6 @@ var recettes = {
       ],
       instructions:
         "Préchauffez votre four à 170°C (thermostat 5-6). Séparez les blancs des jaunes d'oeufs. Dans un saladier, battez les jaunes avec le sucre jusqu'à ce que le mélange blanchisse. Ajoutez le zeste du citron râpé et mélangez. Incorporez la farine et la fécule tamisées ensemble. Montez les blancs en neige avec une pincée de sel et incorporez-les délicatement à la préparation. Versez la pâte dans un moule à manqué beurré et fariné. Enfournez pour 45 minutes. Vérifiez la cuisson avec la pointe d'un couteau, elle doit ressortir sèche. Laissez refroidir avant de démouler.",
-    },
-    {
-      id: "gaufres",
-      nom: "Gaufres moelleuses et croustillantes",
-      ingredients: [
-        "160g de Farine",
-        "4 Oeufs",
-        "200ml de Lait demi-écrémé",
-        "1 sachet de Levure chimique",
-        "1 cuillère à café d'Extrait de vanille liquide ou en poudre",
-        "4 cuillères à café d'Huile de tournesol, pépin de raisin, coco ou beurre fondu",
-        "1 pincée de Sel",
-      ],
-      instructions:
-        "Dans un saladier, mélangez la farine et la levure. Ajoutez les œufs un à un en mélangeant bien après chaque ajout. Ajoutez le lait petit à petit tout en mélangeant pour obtenir une pâte lisse. Ajoutez l'extrait de vanille et l'huile (ou le beurre fondu) et mélangez bien. Laissez reposer la pâte pendant 1 heure. Préchauffez votre gaufrier et huilez-le légèrement. Versez une louche de pâte dans chaque empreinte de gaufre et faites cuire jusqu'à ce que les gaufres soient dorées et croustillantes. Servez chaud avec du sucre glace, de la confiture, du Nutella ou de la chantilly.",
     },
     {
       id: "roussettes",
